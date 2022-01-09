f3127ffc0a491ed221084ed976bd5b0ad5a53191
seungju0000 <seungju0000@naver.com>
2022-01-09 09:37:59 +0900

    add Message for alert

---
+++ src/main/java/carrot/challenge/common/dto/Message.java
@@ -0,0 +1,11 @@
+package carrot.challenge.common.dto;
+
+public class Message {
+    private String message = "";
+    private String href = "";
+
+    public Message(String message, String href) {
+        this.message = message;
+        this.href = href;
+    }
+}
--- src/main/java/carrot/challenge/customer/controller/CustomerController.java
+++ src/main/java/carrot/challenge/customer/controller/CustomerController.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Slf4j
 @Controller
 @RequestMapping("/customer")
@@ -17,8 +20,15 @@ public class CustomerController {
 
     @ResponseBody
     @PostMapping("login")
-    public String login(@RequestParam String email, @RequestParam String password) {
+    public String login(@RequestParam String email, @RequestParam String password, HttpServletResponse response) {
         log.info("email={}, password={}", email, password);
+
+        /*
+        Cookie setCookie = new Cookie("id", "2");
+        setCookie.setMaxAge(60*60*24*30);
+        response.addCookie(setCookie);
+
+        */
         return "post login";
     }
 
@@ -32,6 +42,7 @@ public class CustomerController {
     public String addUser(@ModelAttribute Customer customer) {
         log.info("id={}, email={}, password={}, username={}, phone_number={}, nickname={}",
                 customer.getId(), customer.getEmail(), customer.getPassword(), customer.getUsername(), customer.getPhone_number(), customer.getNickname());
+
         return "post users";
     }
 
